f1de65009e1ab1d17b804133af75cf0c19c9fa79
nawwafhusain@outlook.com <nawwafhusain@outlook.com>
2024-09-10 15:23:10 +0200

    fixed email

--- app/components/contact.js
+++ app/components/contact.js
@@ -2,43 +2,33 @@
 import React from "react";
 import Image from "next/image";
 import { useState, useEffect, useRef } from "react";
-import { toast } from "react-hot-toast";
+// import { toast } from "react-hot-toast";
 import { sendmail } from "@/lib/mail";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 export default function Contact() {
-  //states
-  const [form, setForm] = useState({
-    name: "",
-    inquiry_email: "",
-    company: "",
-    budget: "",
-    message: "",
-  });
+  //react hook form
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submitedWithenTwoMinutes, setSubmitedWithenTwoMinutes] =
     useState(false);
-  const [isValid, setIsValid] = useState(false);
 
-  //Refs
+  // //Refs
   const formRef = useRef();
 
-  const checkFormValidity = () => {
-    const allFieldsFilled = Object.values(form).every(
-      (value) => value.trim() !== ""
-    );
-    setIsValid(allFieldsFilled);
-  };
-
-  useEffect(() => {
-    checkFormValidity();
-  }, [form]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmitFunction = async (formData) => {
     setIsSubmitting(true);
-    const formData = new FormData(e.target);
+    console.log(formData, "formData");
+
     const res = sendmail(formData);
-    //console.log(res, "res");
+    console.log(res, "res");
 
     toast
       .promise(res, {
@@ -63,30 +53,22 @@ export default function Contact() {
         }
       });
 
+    try {
+    } catch (error) {}
+
     setIsSubmitting(false);
     setSubmitedWithenTwoMinutes(true);
     setTimeout(() => {
       setSubmitedWithenTwoMinutes(false);
-      setForm({
-        name: "",
-        inquiry_email: "",
-        company: "",
-        budget: "",
-        message: "",
-      });
-      setIsValid(false);
     }, 120000);
   };
 
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
   return (
     <section
       className=" px-4 lg:px-16 relative overflow-x-clip xl:h-min h-max mt-40 flex justify-center items-center"
       id="contact"
     >
-      <div className="w-full lg:h-[50rem] h-max cards rounded-3xl flex justify-center flex-col items-center lg:gap-8 gap-2 relative ">
+      <div className="w-full  h-max cards rounded-3xl flex justify-center flex-col items-center lg:gap-8 gap-2 relative ">
         <div className="absolute w-full h-full top-0 left-0 overflow-clip rounded-3xl">
           <Image
             src={"/images/3dBlobsRot.png"}
@@ -104,24 +86,62 @@ export default function Contact() {
         </p>
         <div className="lg:w-[50%] w-full h-auto relative">
           <form
-            onSubmit={handleSubmit}
+            onSubmit={handleSubmit(handleSubmitFunction)}
             className="flex flex-col items-center gap-4 w-full"
             ref={formRef}
           >
-            <div className=" w-full flex flex-col lg:flex-row lg:gap-8 gap-2 justify-between">
+            <div
+              className={`${
+                Object.keys(errors).length !== 0 &&
+                errors.constructor === Object
+                  ? "flex-1 bg-white rounded-3xl lg:px-8 py-4 lg:text-xl px-4 text-sm w-full"
+                  : "hidden"
+              } `}
+            >
+              {errors.name && (
+                <span className="text-red-500 text-sm">
+                  Name is required
+                  <br />
+                </span>
+              )}
+              {errors.budget && (
+                <span className="text-red-500 text-sm">
+                  Budget is required
+                  <br />
+                </span>
+              )}
+              {errors.email && (
+                <span className="text-red-500 text-sm">
+                  Email is required
+                  <br />
+                </span>
+              )}
+              {errors.message && (
+                <span className="text-red-500 text-sm">
+                  Message is required <br />
+                </span>
+              )}
+            </div>
+            <div className=" w-full flex flex-col lg:flex-row lg:gap-8 gap-2 justify-between ">
               <input
                 type="text"
                 name="name"
                 placeholder="Name"
                 className="flex-1 lg:rounded-full rounded-3xl lg:px-8 py-4 lg:text-xl px-4 text-sm "
-                onChange={handleChange}
+                {...register("name", {
+                  required: true,
+                })}
               />
+
               <select
                 name="budget"
                 placeholder="budget"
-                defaultValue=""
+                defaultValue="budget"
                 className="lg:w-[40%] lg:rounded-full rounded-3xl lg:px-8 py-4 lg:text-xl px-4 text-sm "
-                onChange={handleChange}
+                {...register("budget", {
+                  required: true,
+                  pattern: /^(?!.*\bBudget\b).*/,
+                })}
               >
                 <option value="">Budget</option>
                 <option value="under 1000">under $1000</option>
@@ -130,34 +150,35 @@ export default function Contact() {
                 <option value="over 5000">over $5000</option>
               </select>
             </div>
+
             <input
               type="email"
-              name="inquiry_email"
+              name="email"
               placeholder="Email"
               className="w-full lg:rounded-full rounded-3xl lg:px-8 py-4 lg:text-xl px-4 text-sm "
-              onChange={handleChange}
+              {...register("email", { required: true })}
             />
+
             <input
               type="text"
               name="company"
               placeholder="Company or Brand Name"
               className="w-full lg:rounded-full rounded-3xl lg:px-8 py-4 lg:text-xl px-4 text-sm "
-              onChange={handleChange}
+              {...register("company")}
             />
+
             <textarea
               name="message"
               placeholder="Please let us know how we can help you"
               className="w-full h-32 lg:rounded-[2rem] rounded-3xl lg:px-8 py-4 lg:text-xl px-4 text-sm "
               rows="6"
-              onChange={handleChange}
+              {...register("message", { required: true })}
             ></textarea>
 
             <button
-              className={` text-sm lg:text-lg  right-1  bg-slate-300 lg:rounded-full rounded-3xl py-4 w-[25%] lg:w-[20%]  ${
-                isValid ? "hover:bg-slate-400" : "opacity-70"
-              }`}
+              className={` text-sm lg:text-lg  right-1  bg-slate-300 lg:rounded-full rounded-3xl py-4 w-[25%] lg:w-[20%] hover:bg-slate-400
+              `}
               type="submit"
-              disabled={isSubmitting || !isValid}
             >
               Submit
             </button>
